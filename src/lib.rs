68dc27bfed1d839dcd05194a706cc2a557190704
ZhangYi <zhangyi@goldensecure.com>
2019-04-02 01:57:18 +0800

    support update gateway

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,10 @@ use actix_web::*;
 use futures::future::Future;
 use ip_addr_op::*;
 use ipnetwork::*;
+use iproute::*;
 use serde::{Deserialize, Serialize};
 
-use std::net::{IpAddr, Ipv4Addr};
+use std::net::Ipv4Addr;
 use std::path::Path;
 
 fn index(_req: &HttpRequest<State>) -> Result<NamedFile> {
@@ -18,7 +19,7 @@ struct Status {
     host_name: &'static str,
     host_ip: String,
     host_mask: String,
-    gateway_ip: &'static str,
+    gateway_ip: String,
     server_ip: &'static str,
     connect_status: &'static str,
     power_status: &'static str,
@@ -41,6 +42,7 @@ impl Responder for Status {
 struct State {
     handle: Handle,
     ifname: String,
+    config_ip: Ipv4Addr,
 }
 
 impl Clone for State {
@@ -48,6 +50,7 @@ impl Clone for State {
         State {
             ifname: self.ifname.clone(),
             handle: self.handle.clone(),
+            config_ip: self.config_ip.clone(),
         }
     }
 }
@@ -56,19 +59,29 @@ fn get_status(req: &HttpRequest<State>) -> impl Responder {
     let (ip_addr, mask) = match get_ip_addrs(req.state().handle.clone(), req.state().ifname.clone())
     {
         Ok(addrs) => {
-            let (addr, prefix) = addrs[1];
-            let mask = Ipv4Network::new(addr, prefix)
-                .expect("parse mask error")
-                .mask();
-            (addr.to_string(), mask.to_string())
+            let (addr, prefix) = addrs[addrs.len() - 1];
+            match Ipv4Network::new(addr, prefix) {
+                Ok(a) => {
+                    let mask = a.mask();
+                    (addr.to_string(), mask.to_string())
+                }
+                Err(e) => {
+                    eprintln!("{}", e);
+                    (String::from(""), String::from(""))
+                }
+            }
         }
         Err(_) => (String::from(""), String::from("")),
+    };
+    let gateway = match get_default_routes() {
+        Ok(ref routes) if routes.len() > 0 => routes[0].gateway().to_string(),
+        _ => String::from(""),
     };
     Status {
         host_name: "***分行人民街分理处",
         host_ip: ip_addr,
         host_mask: mask,
-        gateway_ip: "10.0.8.1",
+        gateway_ip: gateway.to_string(),
         server_ip: "10.0.0.8:8080",
         connect_status: "已连接",
         power_status: "正常",
@@ -86,17 +99,58 @@ struct HostSetting {
 fn set_host(req: &HttpRequest<State>) -> Box<Future<Item = HttpResponse, Error = Error>> {
     let handle = req.state().handle.clone();
     let ifname = req.state().ifname.clone();
+    let config_ip = req.state().config_ip.clone();
     req.json()
         .from_err()
-        .and_then(|val: HostSetting| {
+        .and_then(move |val: HostSetting| {
             let mask = &val.mask;
-            let mask: Ipv4Addr = mask.parse().expect("parse mask error");
-            let prefix: u8 = ipv4_mask_to_prefix(mask).expect("mask to prefix error");
-            let new_ip: Ipv4Addr = (&val.ip).parse().expect("parse ip address error");
-
-            match set_ip_addr(handle, ifname, Ipv4Addr::new(10, 0, 0, 9), new_ip, prefix) {
-                Ok(_) => Ok(HttpResponse::Ok().json(val)), // <- send response
-                Err(_) => Ok(HttpResponse::BadRequest().json(val)),
+            match mask.parse() {
+                Ok(mask) => match (
+                    ipv4_mask_to_prefix(mask),
+                    (&val.ip).parse(),
+                    (&val.gateway).parse(),
+                ) {
+                    (Ok(prefix), Ok(new_ip), Ok(gateway)) => {
+                        // check if gateway is in this ip network
+                        match Ipv4Network::new(new_ip, prefix) {
+                            Ok(network) => {
+                                if !network.contains(gateway) {
+                                    eprintln!("gateway is not in this ip network");
+                                    return Ok(HttpResponse::BadRequest().json(val));
+                                }
+                            }
+                            Err(e) => {
+                                eprintln!("{}", e);
+                                return Ok(HttpResponse::BadRequest().json(val));
+                            }
+                        };
+                        // set host ip address and mask
+                        match set_ip_addr(handle, ifname.clone(), config_ip, new_ip, prefix) {
+                            Ok(_) => {
+                                // update new gateway
+                                match update_default_route(ifname, gateway) {
+                                    Ok(_) => Ok(HttpResponse::Ok().json(val)),
+                                    Err(e) => {
+                                        eprintln!("{}", e);
+                                        Ok(HttpResponse::BadRequest().json(val))
+                                    }
+                                }
+                            }
+                            Err(e) => {
+                                eprintln!("{}", e);
+                                Ok(HttpResponse::BadRequest().json(val))
+                            }
+                        }
+                    }
+                    _ => {
+                        eprintln!("set host ip address and gateway failed");
+                        Ok(HttpResponse::BadRequest().json(val))
+                    }
+                },
+                Err(e) => {
+                    eprintln!("{}", e);
+                    Ok(HttpResponse::BadRequest().json(val))
+                }
             }
         })
         .responder()
@@ -122,7 +176,8 @@ pub fn run() {
     let handle = make_handle();
     let state = State {
         handle,
-        ifname: String::from("enp7s0"),
+        ifname: String::from("eth0"),
+        config_ip: Ipv4Addr::new(10, 0, 0, 14),
     };
     server::new(move || {
         vec![
