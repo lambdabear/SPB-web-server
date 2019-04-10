5d3b57432e1145ba16847d96cf5fc024253ef33a
ZhangYi <zhangyi@goldensecure.com>
2019-04-11 02:28:32 +0800

    support save config info in local config.json file

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use actix_web::fs::NamedFile;
 use actix_web::*;
-use crossbeam_channel::{Receiver, Sender};
+use crossbeam_channel::{bounded, Receiver, Sender};
 use futures::future::Future;
 use ip_addr_op::*;
 use ipnetwork::*;
@@ -15,6 +15,8 @@ use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
+use spb_config::*;
+
 fn index(_req: &HttpRequest<State>) -> Result<NamedFile> {
     let path = Path::new("./index.html");
     Ok(NamedFile::open(path)?)
@@ -22,7 +24,7 @@ fn index(_req: &HttpRequest<State>) -> Result<NamedFile> {
 
 #[derive(Serialize)]
 struct Status {
-    host_name: &'static str,
+    host_name: String,
     host_ip: String,
     host_mask: String,
     gateway_ip: String,
@@ -43,14 +45,42 @@ impl Responder for Status {
     }
 }
 
+#[derive(Clone, Debug)]
+pub struct NetSetting {
+    pub ip_addr: Ipv4Addr,
+    pub prefix: u8,
+    pub gateway: Ipv4Addr,
+}
+
+impl NetSetting {
+    fn new(ip_addr: Ipv4Addr, prefix: u8, gateway: Ipv4Addr) -> Self {
+        NetSetting {
+            ip_addr,
+            prefix,
+            gateway,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum SaveMsg {
+    DeviceName(String),
+    Net(NetSetting),
+    Broker(Broker),
+}
+
 #[derive(Clone)]
 struct State {
+    config_path: String,
+    device_name: Arc<Mutex<String>>,
     handle: Handle,
     ifname: String,
     config_ip: Ipv4Addr,
     status_buffer: Arc<Mutex<VecDeque<String>>>,
     broker: Arc<Mutex<Broker>>,
     broker_set_s: Sender<Broker>,
+    save_s: Sender<SaveMsg>,
+    device_name_s: Sender<String>,
 }
 
 fn get_status(req: &HttpRequest<State>) -> impl Responder {
@@ -58,6 +88,7 @@ fn get_status(req: &HttpRequest<State>) -> impl Responder {
     {
         Ok(addrs) => addrs
             .iter()
+            .filter(|(addr, _)| *addr != req.state().config_ip)
             .map(|(addr, prefix)| match Ipv4Network::new(*addr, *prefix) {
                 Ok(a) => (addr.to_string(), a.mask().to_string()),
                 Err(_) => ("".to_string(), "".to_string()),
@@ -90,8 +121,15 @@ fn get_status(req: &HttpRequest<State>) -> impl Responder {
         }),
         Err(_) => "".to_string(),
     };
+    let host_name = match req.state().device_name.lock() {
+        Ok(name) => name.clone(),
+        Err(e) => {
+            eprintln!("{}", e);
+            "".to_string()
+        }
+    };
     Status {
-        host_name: "***分行人民街分理处",
+        host_name,
         host_ip: ip_addr,
         host_mask: mask,
         gateway_ip: gateway.to_string(),
@@ -101,11 +139,15 @@ fn get_status(req: &HttpRequest<State>) -> impl Responder {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct HostSetting {
-    name: String,
+struct NetworkSetting {
     ip: String,
     mask: String,
     gateway: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct NameSetting {
+    name: String,
 }
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
@@ -125,13 +167,14 @@ impl Broker {
     }
 }
 
-fn set_host(req: &HttpRequest<State>) -> Box<Future<Item = HttpResponse, Error = Error>> {
+fn set_net(req: &HttpRequest<State>) -> Box<Future<Item = HttpResponse, Error = Error>> {
     let handle = req.state().handle.clone();
     let ifname = req.state().ifname.clone();
     let config_ip = req.state().config_ip.clone();
+    let save_s = req.state().save_s.clone();
     req.json()
         .from_err()
-        .and_then(move |val: HostSetting| {
+        .and_then(move |val: NetworkSetting| {
             let mask = &val.mask;
             match mask.parse() {
                 Ok(mask) => match (
@@ -158,7 +201,16 @@ fn set_host(req: &HttpRequest<State>) -> Box<Future<Item = HttpResponse, Error =
                             Ok(_) => {
                                 // update new gateway
                                 match update_default_route(ifname, gateway) {
-                                    Ok(_) => Ok(HttpResponse::Ok().json(val)),
+                                    Ok(_) => {
+                                        // send saving to config file message
+                                        match save_s.send(SaveMsg::Net(NetSetting::new(
+                                            new_ip, prefix, gateway,
+                                        ))) {
+                                            Ok(()) => (),
+                                            Err(e) => eprintln!("{}", e),
+                                        };
+                                        Ok(HttpResponse::Ok().json(val))
+                                    }
                                     Err(e) => {
                                         eprintln!("{}", e);
                                         Ok(HttpResponse::BadRequest().json(val))
@@ -185,51 +237,85 @@ fn set_host(req: &HttpRequest<State>) -> Box<Future<Item = HttpResponse, Error =
         .responder()
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct MqttServerSetting {
-    host: String,
-    port: u16,
-}
-
 fn set_mqtt_server(req: &HttpRequest<State>) -> Box<Future<Item = HttpResponse, Error = Error>> {
     let broker_set_s = req.state().broker_set_s.clone();
     req.json()
         .from_err()
-        .and_then(
-            move |val: MqttServerSetting| match Broker::new(val.host, val.port) {
-                Ok(broker) => match broker_set_s.send(broker) {
-                    Ok(()) => Ok(HttpResponse::Ok().json("")),
-                    Err(e) => {
-                        println!("{:?}", e);
-                        Ok(HttpResponse::BadRequest().json("message sending error"))
-                    }
-                },
-                Err(e) => Ok(HttpResponse::BadRequest().json(e)),
-            },
-        )
+        .and_then(move |val: Broker| match broker_set_s.send(val) {
+            Ok(()) => Ok(HttpResponse::Ok().json("")),
+            Err(e) => {
+                eprintln!("{:?}", e);
+                Ok(HttpResponse::BadRequest().json("message sending error"))
+            }
+        })
         .responder()
 }
 
-pub fn run(r: Receiver<Vec<u8>>, broker_r: Receiver<Broker>, broker_set_s: Sender<Broker>) {
+fn set_name(req: &HttpRequest<State>) -> Box<Future<Item = HttpResponse, Error = Error>> {
+    let save_s = req.state().save_s.clone();
+    let device_name_s = req.state().device_name_s.clone();
+    req.json()
+        .from_err()
+        .and_then(move |val: NameSetting| {
+            // request to save new device name to config file
+            match save_s.send(SaveMsg::DeviceName(val.name.clone())) {
+                Ok(()) => {
+                    // request to change state's device name
+                    match device_name_s.send(val.name) {
+                        Ok(()) => (),
+                        Err(e) => eprintln!("{}", e),
+                    }
+                    Ok(HttpResponse::Ok().json(""))
+                }
+                Err(e) => {
+                    eprintln!("{}", e);
+                    Ok(HttpResponse::BadRequest().json("message sending error"))
+                }
+            }
+        })
+        .responder()
+}
+
+pub fn run(
+    r: Receiver<Vec<u8>>,
+    broker_r: Receiver<Broker>,
+    broker_set_s: Sender<Broker>,
+    save_s: Sender<SaveMsg>,
+) {
+    let config_path = "./config.json".to_string();
+    let config = match read_config(&config_path) {
+        Ok(conf) => conf,
+        Err(e) => {
+            eprintln!("{}", e);
+            Config::init_config_val()
+        }
+    };
+    let device_name = Arc::new(Mutex::new(config.device_name));
     let handle = make_handle();
     let status_buffer = Arc::new(Mutex::new(VecDeque::with_capacity(5)));
     let status_b = status_buffer.clone();
-    let init_broker = Broker::new("localhost".to_string(), 1883).expect("init_broker invalid");
+    let init_broker =
+        Broker::new(config.broker_host, config.broker_port).expect("init_broker invalid");
     let broker = Arc::new(Mutex::new(init_broker));
     let broker1 = broker.clone();
+    let (device_name_s, device_name_r) = bounded(1);
     let state = State {
+        config_path,
+        device_name: device_name.clone(),
         handle,
         ifname: String::from("eth0"),
         config_ip: Ipv4Addr::new(10, 188, 188, 188),
         status_buffer,
         broker,
         broker_set_s,
+        save_s,
+        device_name_s,
     };
 
-    // save receive message to status buffer every 100ms
+    // save receive message to status buffer and save new device name
     thread::spawn(move || loop {
         thread::sleep(Duration::from_millis(100));
-        match r.recv() {
+        match r.try_recv() {
             Ok(msg) => match String::from_utf8(msg) {
                 Ok(s) => match status_b.lock() {
                     Ok(mut status) => {
@@ -244,11 +330,18 @@ pub fn run(r: Receiver<Vec<u8>>, broker_r: Receiver<Broker>, broker_set_s: Sende
                 },
                 Err(e) => eprintln!("{}", e),
             },
+            Err(_) => (),
+        }
+        match device_name_r.try_recv() {
+            Ok(name) => match device_name.lock() {
+                Ok(mut dev_name) => *dev_name = name,
+                Err(e) => eprintln!("{}", e),
+            },
             Err(e) => eprintln!("{}", e),
         }
     });
 
-    // get updated broker message every 1 second
+    // get updated broker info message every 1 second
     thread::spawn(move || loop {
         thread::sleep(Duration::from_secs(1));
         match broker_r.recv() {
@@ -265,8 +358,9 @@ pub fn run(r: Receiver<Vec<u8>>, broker_r: Receiver<Broker>, broker_set_s: Sende
             App::with_state(state.clone())
                 .prefix("/api")
                 .resource("/status", |r| r.f(get_status))
-                .resource("/setHost", |r| r.f(set_host))
-                .resource("/setMqttServer", |r| r.f(set_mqtt_server)),
+                .resource("/setNet", |r| r.f(set_net))
+                .resource("/setMqttServer", |r| r.f(set_mqtt_server))
+                .resource("/setName", |r| r.f(set_name)),
             App::with_state(state.clone()).resource("/", |r| r.f(index)),
         ]
     })
